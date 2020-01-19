6f002128e2f9ca2a5e1f9df38cfef0cdc318b6fd
Dwi Satria <dwisatriakusuma4@gmail.com>
2020-01-19 13:47:28 +0700

    Update Card Docs

--- src/components/Card/index.jsx
+++ src/components/Card/index.jsx
@@ -6,7 +6,15 @@ import imagesLink from '../../utils/images';
 import { FontAwesomeIcon } from '@fortawesome/react-fontawesome'
 import { faTrash } from '@fortawesome/free-solid-svg-icons'
 
-const Card = ( props ) => {
+/**
+ * Currency Card component
+ * @param {Object} props - react props
+ * @param {string} props.currencyTo - currency target
+ * @param {string} props.currencyFrom - currency from
+ * @param {number} props.amount - currenct amount 
+ * @param {number} props.rate - currency_from and currency_to rate
+ */
+const Card = ({ currencyTo, currencyFrom, amount, rate, onDeleteHandler }) => {
   return (
     <div 
       className="shadow-md md:w-2/4 sm:w-full lg:w-2/5 my-3 flex rounded-lg"
@@ -17,24 +25,24 @@ const Card = ( props ) => {
           <div className="currency-amount__code">
             <div className="currency-icon">
               <img className="currency-icon-image" 
-                src={imagesLink[props.currency_to]} 
-                alt={ getCurrencyName(props.currency_to) }
+                src={imagesLink[currencyTo]} 
+                alt={ getCurrencyName(currencyFrom) }
               />
             </div>
-            <span className="currency-amount-__code__text">{ props.currency_to }</span>
+            <span className="currency-amount-__code__text">{ currencyTo }</span>
           </div>
-          <div className="currency-amount__converted-amount">{ props.currency_amount }</div>
+          <div className="currency-amount__converted-amount">{ amount }</div>
         </div>
 
         <div className="row col-sm-12 col-md-12 col-lg-12 currency-name">
           <div className="currency-name__code">
-            <p>{ props.currency_to } - { getCurrencyName(props.currency_to)}</p>
+            <p>{ currencyTo } - { getCurrencyName(currencyTo)}</p>
           </div>
         </div>
 
         <div className="rate">
           <div className="rate__amount">
-            <p>1 { props.currency_from } = { props.currency_to } { props.rate }</p> 
+            <p>1 { currencyFrom } = { currencyTo } { rate }</p> 
           </div>
         </div>
       </div>
@@ -42,7 +50,7 @@ const Card = ( props ) => {
       {/* BUTTON DELETE */}
       <div 
         className="button-container"
-        onClick={ () => props.onDeleteHandler( props.currency_to ) }
+        onClick={ () => onDeleteHandler( currencyTo ) }
       >
         <FontAwesomeIcon icon={faTrash} className="btn-delete" />
       </div>
@@ -52,21 +60,19 @@ const Card = ( props ) => {
 }
 
 Card.propTypes = {
-  currency_amount: PropTypes.number,
-  currency_to_name: PropTypes.string,
-  currency_from: PropTypes.string,
-  currency_to: PropTypes.string,
-  rate: PropTypes.number,
-  onDeleteHandler: PropTypes.func
+  currencyTo: PropTypes.string, 
+  currencyFrom: PropTypes.string, 
+  amount: PropTypes.number, 
+  rate: PropTypes.number, 
+  onDeleteHandler: PropTypes.func,
 }
 
 Card.defaultProps = {
-  currency_amount: 123000,
-  currency_to_name: 'Indonesia Rupiah',
-  currency_from: 'USD',
-  currency_to: 'IDR',
-  rate: 14000,
-  onDeleteHandler: null
+  currencyTo: 'IDR', 
+  currencyFrom: 'USD', 
+  amount: 0, 
+  rate: 0, 
+  onDeleteHandler: () => console.log('Delete Hanlder'),
 }
 
 export default Card;
